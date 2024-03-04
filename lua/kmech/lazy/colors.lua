2cdfd03129adba9878736963f8e30ad63217ba57
Krzysztof Mech <krzysztofmech7@gmail.com>
2024-03-04 19:18:02 +0100

    add themes

--- lua/kmech/lazy/colors.lua
+++ lua/kmech/lazy/colors.lua
@@ -33,6 +33,10 @@ return {
         config = function()
             require("rose-pine").setup({
                 extend_background_behind_borders = true,
+
+                styles = {
+                    transparency = true,
+                }
             })
             vim.cmd("colorscheme rose-pine")
         end
