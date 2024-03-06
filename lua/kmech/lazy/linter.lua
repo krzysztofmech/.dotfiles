59c7dd9e90a3fb3337f4e57ba4ba98dd43ce9b5a
Krzysztof Mech <krzysztof.mech@grupawp.pl>
2024-03-06 09:35:41 +0100

    change formatters

--- lua/kmech/lazy/linter.lua
+++ lua/kmech/lazy/linter.lua
@@ -1,93 +1,35 @@
--- return {
---     "mfussenegger/nvim-lint",
---     event = {
---         "BufReadPre",
---         "BufNewFile"
---     },
---     config = function()
---         local lint = require("lint")
---
---         lint.linters_by_ft = {
---             javascript = { "eslint_d" },
---             typescript = { "eslint_d" },
---             javascriptvue = { "eslint_d" },
---             typescriptvue = { "eslint_d" },
---             javascriptreact = { "eslint_d" },
---             typescriptreact = { "eslint_d" },
---             css = { "eslint_d" },
---             html = { "eslint_d" },
---             json = { "eslint_d" },
---             graphql = { "eslint_d" },
---             golang = { "golangci_lint_ls" },
---         }
---         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
---
---         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
---             group = lint_augroup,
---             callback = function()
---                 lint.try_lint()
---             end,
---         })
---         vim.keymap.set("n", "<leader>l", function()
---             lint.try_lint()
---         end, { desc = "Trigger linting for current file" })
---     end
--- }
 return {
-    "jose-elias-alvarez/null-ls.nvim",
+    "mfussenegger/nvim-lint",
+    event = {
+        "BufReadPre",
+        "BufNewFile"
+    },
     config = function()
-        local null_ls = require("null-ls")
+        local lint = require("lint")
 
-        local root_has_file = function(files)
-            return function(utils)
-                return utils.root_has_file(files)
-            end
-        end
-
-        local eslint_root_files = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
-        local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
-        local stylua_root_files = { "stylua.toml", ".stylua.toml" }
-        local elm_root_files = { "elm.json" }
-
-        local opts = {
-            eslint_formatting = {
-                condition = function(utils)
-                    local has_eslint = root_has_file(eslint_root_files)(utils)
-                    local has_prettier = root_has_file(prettier_root_files)(utils)
-                    return has_eslint and not has_prettier
-                end,
-            },
-            eslint_diagnostics = {
-                condition = root_has_file(eslint_root_files),
-            },
-            prettier_formatting = {
-                condition = root_has_file(prettier_root_files),
-            },
-            stylua_formatting = {
-                condition = root_has_file(stylua_root_files),
-            },
-            elm_format_formatting = {
-                condition = root_has_file(elm_root_files),
-            },
+        lint.linters_by_ft = {
+            javascript = { "eslint_d" },
+            typescript = { "eslint_d" },
+            javascriptvue = { "eslint_d" },
+            typescriptvue = { "eslint_d" },
+            javascriptreact = { "eslint_d" },
+            typescriptreact = { "eslint_d" },
+            css = { "eslint_d" },
+            html = { "eslint_d" },
+            json = { "eslint_d" },
+            graphql = { "eslint_d" },
+            golang = { "golangci_lint_ls" },
         }
+        local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-        local function on_attach(client, _)
-            if client.server_capabilities.document_formatting then
-                vim.cmd("command! -buffer Formatting lua vim.lsp.buf.formatting()")
-                vim.cmd("command! -buffer FormattingSync lua vim.lsp.buf.formatting_sync()")
-            end
-        end
-
-        null_ls.setup({
-            sources = {
-                null_ls.builtins.diagnostics.eslint_d.with(opts.eslint_diagnostics),
-                null_ls.builtins.formatting.eslint_d.with(opts.eslint_formatting),
-                null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
-                null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
-                null_ls.builtins.formatting.elm_format.with(opts.elm_format_formatting),
-                null_ls.builtins.code_actions.eslint_d.with(opts.eslint_diagnostics),
-            },
-            on_attach = on_attach,
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
+            group = lint_augroup,
+            callback = function()
+                lint.try_lint()
+            end,
         })
-    end,
+        vim.keymap.set("n", "<leader>l", function()
+            lint.try_lint()
+        end, { desc = "Trigger linting for current file" })
+    end
 }
