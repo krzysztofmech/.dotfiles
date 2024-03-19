40cf98d43d945c78f9fb9b69bad0a6a10a197765
Krzysztof Mech <krzysztof.mech@grupawp.pl>
2024-03-19 19:46:19 +0100

    add keymaps

--- nvim/lua/kmech/keymaps.lua
+++ nvim/lua/kmech/keymaps.lua
@@ -9,27 +9,30 @@ keymap("v", "K", ":m '<-2<CR>gv=gv")
 keymap("n", "<leader>w", ":vsplit<CR><C-w>w", { desc = "Window split vertically" })
 
 -- move windows
-keymap('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-keymap('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-keymap('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-keymap('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+keymap("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+keymap("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+keymap("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+keymap("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 -- open file explorer
 keymap("n", "<leader>b", ":Neotree<CR>", { desc = "Open file explorer" })
 
 -- open terminal
 keymap("n", "<leader>to", function()
-    vim.cmd("botright split")
-    vim.cmd("terminal")
+	vim.cmd("botright split")
+	vim.cmd("terminal")
 end, { desc = "[T]erminal [O]pen" })
 
 -- terminal mode
-keymap('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+keymap("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 
 -- vertical motions
 keymap("n", "<C-d>", "<C-d>zz")
 keymap("n", "<C-u>", "<C-u>zz")
 
 -- paste
-keymap("x", "<leader>p", "\"_dP")
+keymap("x", "<leader>p", '"_dP')
 -- replace all
 keymap("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "[R]eplace cursor" })
+-- git diff get
+keymap("n", "<leader>gh", "<cmd>diffget //2<CR>")
+keymap("n", "<leader>gl", "<cmd>diffget //3<CR>")
--- nvim/lua/kmech/lazy/git.lua
+++ nvim/lua/kmech/lazy/git.lua
@@ -1,35 +1,37 @@
 return {
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("gitsigns").setup({
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
 
-                current_line_blame = true,
-                current_line_blame_opts = {
-                    delay = 1000,
-                },
-                on_attach = function(bufnr)
-                    local gs = package.loaded.gitsigns
-                    local function map(mode, l, r, opts)
-                        opts = opts or {}
-                        opts.buffer = bufnr
-                        vim.keymap.set(mode, l, r, opts)
-                    end
-                    map("n", "<leader>gp", gs.preview_hunk, { desc = "[G]it inline changes [p]review" })
-                    map("n", "<leader>gs", gs.stage_hunk, { desc = "[G]it [s]tage changes hunk" })
-                    map("n", "<leader>gr", gs.reset_hunk, { desc = "[G]it [r]eset staged changes" })
-                    map("v", "<leader>gs", function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end,
-                        { desc = "[G]it [s]tage changes hunk visual" })
-                    map("v", "<leader>gr", function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end,
-                        { desc = "[G]it [r]eset changes hunk visual" })
-                    map("n", "<leader>gS", gs.stage_buffer, { desc = "[G]it [s]tage changes buffer" })
-                    map("n", "<leader>gR", gs.reset_buffer, { desc = "[G]it [r]eset staged changes buffer" })
-                    map("n", "<leader>gd", gs.diffthis, { desc = "[G]it [d]iff" })
-                end
-            })
-        end
-    },
-    {
-        "tpope/vim-fugitive",
-    }
+				current_line_blame = true,
+				current_line_blame_opts = {
+					delay = 1000,
+				},
+				on_attach = function(bufnr)
+					local gs = package.loaded.gitsigns
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+					map("n", "<leader>gp", gs.preview_hunk, { desc = "[G]it inline changes [p]review" })
+					map("n", "<leader>gs", gs.stage_hunk, { desc = "[G]it [s]tage changes hunk" })
+					map("n", "<leader>gr", gs.reset_hunk, { desc = "[G]it [r]eset staged changes" })
+					map("v", "<leader>gs", function()
+						gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "[G]it [s]tage changes hunk visual" })
+					map("v", "<leader>gr", function()
+						gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "[G]it [r]eset changes hunk visual" })
+					map("n", "<leader>gS", gs.stage_buffer, { desc = "[G]it [s]tage changes buffer" })
+					map("n", "<leader>gR", gs.reset_buffer, { desc = "[G]it [r]eset staged changes buffer" })
+					map("n", "<leader>gd", gs.diffthis, { desc = "[G]it [d]iff" })
+				end,
+			})
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+	},
 }
