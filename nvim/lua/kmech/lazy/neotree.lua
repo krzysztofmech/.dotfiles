ee4da8832a8b877228cebad4d8d58cae4362c72e
Krzysztof Mech <krzysztof.mech@grupawp.pl>
2024-03-22 17:47:41 +0100

    follow current file

--- nvim/lua/kmech/lazy/neotree.lua
+++ nvim/lua/kmech/lazy/neotree.lua
@@ -1,30 +1,31 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-        "MunifTanjim/nui.nvim",
-        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    config = function()
-        vim.keymap.set("n", "<leader>b", ":Neotree<CR>")
-        require("neo-tree").setup({
-            update_focused_file = {
-                enable = true
-            },
-            filesystem = {
-                filtered_items = {
-                    visible = true,
-                    show_hidden_count = true,
-                    hide_dotfiles = false,
-                    hide_gitignored = false,
-                    hide_by_name = {
-                        '.git'
-                    },
-                    never_show = {},
-                }
-            }
-        })
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>b", ":Neotree<CR>")
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						".git",
+					},
+					never_show = {},
+				},
+				follow_current_file = {
+					enabled = true,
+					leave_dirs_open = false,
+				},
+			},
+		})
+	end,
 }
