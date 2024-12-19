d46977d326a9861a7a9cd2c1e894ef7d77b17773
Krzysztof Mech <krzysztof.mech@grupawp.pl>
2024-12-19 16:51:51 +0100

    fix telescope find_files

--- nvim/lua/kmech/lazy/telescope.lua
+++ nvim/lua/kmech/lazy/telescope.lua
@@ -4,12 +4,14 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--files<cr>", {})
-		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
+		vim.keymap.set("n", "<leader>ff", function()
+			builtin.find_files({ find_command = { "rg", "--files", "--hidden", "-g", "!.git" } })
+		end, { desc = "Telescope [f]ind [f]iles" })
+		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Telescope [f]ind [d]iagnostics" })
 		vim.keymap.set("n", "<leader>fg", function()
 			builtin.live_grep({ additional_args = { "--fixed-strings" } })
-		end, {})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+		end, { desc = "Telescope [f]ind [g]rep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope [f]ind [b]uffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope [f]ind [h]elp" })
 	end,
 }
