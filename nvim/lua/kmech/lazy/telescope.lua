e66d0c6242ded22d747fbc79f82d07d82108bb50
Krzysztof Mech <krzysztofmech7@gmail.com>
2024-12-19 16:37:20 +0100

    filter out hidden files

--- nvim/lua/kmech/lazy/telescope.lua
+++ nvim/lua/kmech/lazy/telescope.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--hidden,--files<cr>", {})
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files find_command=rg,--ignore,--files<cr>", {})
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, {})
 		vim.keymap.set("n", "<leader>fg", function()
 			builtin.live_grep({ additional_args = { "--fixed-strings" } })
